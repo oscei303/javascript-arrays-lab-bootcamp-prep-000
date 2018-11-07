373f631b58e906b21b3ab59878c949c00f25f981
OSCAR RIOS <oscr.rss@gmail.com>
2018-11-07 21:18:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function appendKitten(name){
 function prependKitten(name){
   return [name, ...kittens]
 }
-function removeLastKitten(kittens){
+function removeLastKitten(){
   return kittens.slice(0, kittens.length - 1)
 }
 function removeFirstKitten(){
